e34719603e08c8dc7c4b06846de7effed75ab4a2
RafiAkem <yc66zio@gmail.com>
2024-09-18 02:13:46 +0700

    SpeedInsights & Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import Footer from "@/components/Footer";
 import FloatingWhatsApp from "@/components/FloatingWhatsApp";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "EasyIzin - Solusi Bisnis Terbaik",
@@ -27,6 +29,8 @@ export default function RootLayout({
         {children}
         <Footer />
         <FloatingWhatsApp />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
